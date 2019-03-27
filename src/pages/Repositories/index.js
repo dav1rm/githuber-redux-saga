ea8d2664b4c026a57c586d0b963b6de56c0eb74f
Davi <davirodrigues8@gmail.com>
2019-03-26 22:28:39 -0300

    APlica estilo na listagem de repositorios

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Text, ActivityIndicator } from 'react-native';
+import { ScrollView, ActivityIndicator } from 'react-native';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Creators as RepositoriesActions } from '~/store/ducks/repositories';
 
-import { Container } from './styles';
+import {
+  Container, Header, HeaderText, Item, ItemName, ItemDescription,
+} from './styles';
 
 class Repositories extends Component {
   componentDidMount() {
@@ -14,16 +16,32 @@ class Repositories extends Component {
     loadRepositoriesRequest();
   }
 
+  renderRepositories = () => {
+    const { repositories } = this.props;
+
+    return repositories.data.map(repo => (
+      <Item key={repo.id}>
+        <ItemName>{repo.name}</ItemName>
+        <ItemDescription>{repo.description}</ItemDescription>
+      </Item>
+    ));
+  };
+
   render() {
     const { repositories } = this.props;
 
     return (
       <Container>
-        {repositories.loading ? (
-          <ActivityIndicator size="small" color="#999" />
-        ) : (
-          repositories.data.map(repo => <Text key={repo.id}>{repo.name}</Text>)
-        )}
+        <Header>
+          <HeaderText>Meus Repositórios</HeaderText>
+        </Header>
+        <ScrollView>
+          {repositories.loading ? (
+            <ActivityIndicator size="small" color="#999" />
+          ) : (
+            this.renderRepositories()
+          )}
+        </ScrollView>
       </Container>
     );
   }
--- src/pages/Repositories/styles.js
+++ src/pages/Repositories/styles.js
@@ -2,7 +2,41 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background: #fff;
-  align-items: center;
+  background: #eee;
+  align-items: stretch;
   justify-content: center;
+`;
+
+export const Header = styled.View`
+  background: #fff;
+  justify-content: center;
+  align-items: center;
+  height: 58px;
+  border-bottom-width: 1px;
+  border-bottom-color: #cecece;
+`;
+
+export const HeaderText = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: #333;
+`;
+
+export const Item = styled.View`
+  flex: 1;
+  background: #fff;
+  margin: 10px 10px 0 10px;
+  padding: 10px;
+  border-radius: 4px;
+`;
+
+export const ItemName = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: #333;
+  padding-bottom: 5px;
+`;
+export const ItemDescription = styled.Text`
+  font-size: 12px;
+  color: #666;
 `;
